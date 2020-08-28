f019d8e0760b92ffc4af82db8267e82847878f6b
lgw <privateli@qq.com>
2020-08-28 22:37:38 +0800

    fix: 强杀任务后go进程仍然不退出的bug(Linux平台)

--- worker/Executor.go
+++ worker/Executor.go
@@ -1,9 +1,12 @@
 package worker
 
 import (
+	"bytes"
+	"context"
 	"github.com/imlgw/scheduler/common"
 	"math/rand"
 	"os/exec"
+	"syscall"
 	"time"
 )
 
@@ -47,7 +50,8 @@ func (executor *Executor) ExecutorJob(info *common.JobExecuteInfo) {
 			//创建shell命令，传入cancelCtx
 			cmd = exec.CommandContext(info.CancelCtx, G_config.BashPath, "-c", info.Job.Command)
 			//执行并捕获输出
-			output, err = cmd.CombinedOutput()
+			//fix: 强杀任务不正常返回 参考：http://imlgw.top/2020/08/25/golang-cai-keng-exec-qu-xiao-bu-tui-chu/
+			output, err = RunCmd(info.CancelCtx, cmd)
 			//记录任务结束时间
 			result.EndTime = time.Now()
 			result.Output = output
@@ -56,6 +60,39 @@ func (executor *Executor) ExecutorJob(info *common.JobExecuteInfo) {
 		//任务执行完成后，把执行的结果返回给Scheduler，Scheduler会从executeTable删除任务
 		G_scheduler.PushJobResult(result)
 	}()
+}
+
+//fix: 强杀任务不正常返回
+func RunCmd(ctx context.Context, cmd *exec.Cmd) ([]byte, error) {
+	var (
+		b   bytes.Buffer
+		err error
+	)
+	//开辟新的线程组（Linux平台特有的属性）
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Setpgid: true, //使得Shell进程开辟新的PGID,即Shell进程的PID,它后面创建的所有子进程都属于该进程组
+	}
+	cmd.Stdout = &b
+	cmd.Stderr = &b
+	if err = cmd.Start(); err != nil {
+		return nil, err
+	}
+	var finish = make(chan struct{})
+	defer close(finish)
+	go func() {
+		select {
+		case <-ctx.Done(): //超时/被cancel 结束
+			//kill -(-PGID)杀死整个进程组
+			syscall.Kill(-cmd.Process.Pid, syscall.SIGKILL)
+		case <-finish: //正常结束
+		}
+	}()
+	//wait等待goroutine执行完，然后释放FD资源
+	//这个时候再kill掉shell进程就不会再等待了，会直接返回
+	if err = cmd.Wait(); err != nil {
+		return nil, err
+	}
+	return b.Bytes(), err
 }
 
 //初始化执行器
